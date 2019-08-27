01526bc6a606b256c837c2b23517e8820266f9ae
Bohdan <bodya.flesh@gmail.com>
2019-08-27 22:30:06 +0300

    update

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -24,6 +24,14 @@
                 <input type="text" name="email" value="{{ old('email') }}" class="form-control">
                 <div>{{ $errors->first('email') }}</div>
             </div>
+            <div class="form-group">
+                <label for="">Status</label>
+                <select name="active" id="active" class="form-control">
+                    <option value="" disabled>Select customer status</option>
+                    <option value="1">Active</option>
+                    <option value="0">Inactive</option>
+                </select>
+            </div>
             
             <button class="btn btn-primary" type="submit">Submit</button>
         </form>
@@ -32,9 +40,18 @@
 
 <hr>
 <div class="row">
-    <div class="col-12">
+    <div class="col-6">
+        <h3>Active customers</h3>
         <ul>
-            @foreach ($customers as $customer)
+            @foreach ($activeCustomers as $customer)
+                <li>{{ $customer->name }} ({{ $customer->email }})</li>
+            @endforeach
+        </ul>
+    </div>
+    <div class="col-6">
+            <h3>Inactive customers</h3>
+        <ul>
+            @foreach ($inactiveCustomers as $customer)
                 <li>{{ $customer->name }} ({{ $customer->email }})</li>
             @endforeach
         </ul>
