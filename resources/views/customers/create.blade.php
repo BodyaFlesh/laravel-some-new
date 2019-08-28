46fed0888865fb6f8c3e61ce6258e10edb0f2c33
Bohdan <bodya.flesh@gmail.com>
2019-08-28 22:11:44 +0300

    update controller

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -13,33 +13,7 @@
 <div class="row">
     <div class="col-12">
         <form action="/customers" method="POST" class="pb-5">
-            @csrf
-            <div class="form-group">
-                <label for="">Name:</label>
-                <input type="text" name="name" value="{{ old('name') }}" class="form-control">
-                <div>{{ $errors->first('name') }}</div>
-            </div>
-            <div class="form-group">
-                <label for="">Email:</label>
-                <input type="text" name="email" value="{{ old('email') }}" class="form-control">
-                <div>{{ $errors->first('email') }}</div>
-            </div>
-            <div class="form-group">
-                <label for="">Status</label>
-                <select name="active" id="active" class="form-control">
-                    <option value="" disabled>Select customer status</option>
-                    <option value="1">Active</option>
-                    <option value="0">Inactive</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="">Company</label>
-                <select name="company_id" id="company_id" class="form-control">
-                    @foreach($companies as $company)
-                        <option value="{{ $company->id }}">{{ $company->name }}</option>
-                    @endforeach
-                </select>
-            </div>
+            @include('customers.form')
             
             <button class="btn btn-primary" type="submit">Submit</button>
         </form>
---
+++ resources/views/customers/edit.blade.php
@@ -0,0 +1,24 @@
+@extends('layout')
+
+@section('title', 'Edit details'.$customer->name)
+
+@section('content')
+
+<div class="row">
+    <div class="col-12">
+        <h1>Edit details {{ $customer->name }}</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-12">
+        <form action="/customers/{{ $customer->id }}" method="POST" class="pb-5">
+            @method('PATCH')
+            @include('customers.form')
+            
+            <button class="btn btn-primary" type="submit">Save customer</button>
+        </form>
+    </div>
+</div>
+
+@endsection
